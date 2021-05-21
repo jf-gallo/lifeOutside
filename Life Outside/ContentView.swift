7a782016f8685396bee4ee653431ed1662103981
Jonatan Contreras <jcontreras@xumak.com>
2021-05-21 11:47:37 -0600

    Initial Commit

---
+++ Life Outside/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Life Outside
+//
+//  Created by Jonatan Contreras on 21/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Life Outside/Life_OutsideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_OutsideApp.swift
+//  Life Outside
+//
+//  Created by Jonatan Contreras on 21/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Life_OutsideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
